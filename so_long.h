6252b8af9e8e7e73112fbc98625970fbf468cb57
Selman Demir <34183418+Selmand42@users.noreply.github.com>
2024-03-31 14:08:51 +0300

    Update so_long.h

--- so_long.h
+++ so_long.h
@@ -15,8 +15,6 @@
 
 # include <unistd.h>
 # include <fcntl.h>
-# include <errno.h>
-# include <string.h>
 # include "get_next_line/get_next_line.h"
 # include "minilibx/mlx.h"
 # include "ft_printf/ft_printf.h"
